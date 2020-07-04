2553ed6c9747e937c1dbeac36591a99a5d0a50c0
DraZiiXx <draziix01@gmail.com>
2020-07-04 17:43:05 +0200

    Update landing.js

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -679,9 +679,9 @@ function dlAsync(login = true){
                 const gameStateChange = function(data){
                     data = data.trim()
                     if(SERVER_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Exploring the Realm!')
+                        DiscordWrapper.updateDetails('En exploration!')
                     } else if(GAME_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Sailing to Westeros!')
+                        DiscordWrapper.updateDetails('Rejoins Revive!')
                     }
                 }
 
