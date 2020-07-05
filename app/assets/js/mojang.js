a665dc6130848d9ca82191a680d4b71bd82d9f3a
DraZiiXx <draziix01@gmail.com>
2020-07-05 18:41:22 +0200

    Update mojang.js

--- app/assets/js/mojang.js
+++ app/assets/js/mojang.js
@@ -9,50 +9,7 @@
 const request = require('request')
 const logger  = require('./loggerutil')('%c[Mojang]', 'color: #a02d2a; font-weight: bold')
 
-// Constants
-const minecraftAgent = {
-    name: 'Minecraft',
-    version: 1
-}
-const authpath = 'https://authserver.mojang.com'
-const statuses = [
-    {
-        service: 'sessionserver.mojang.com',
-        status: 'grey',
-        name: 'Multiplayer Session Service',
-        essential: true
-    },
-    {
-        service: 'authserver.mojang.com',
-        status: 'grey',
-        name: 'Authentication Service',
-        essential: true
-    },
-    {
-        service: 'textures.minecraft.net',
-        status: 'grey',
-        name: 'Minecraft Skins',
-        essential: false
-    },
-    {
-        service: 'api.mojang.com',
-        status: 'grey',
-        name: 'Public API',
-        essential: false
-    },
-    {
-        service: 'minecraft.net',
-        status: 'grey',
-        name: 'Minecraft.net',
-        essential: false
-    },
-    {
-        service: 'account.mojang.com',
-        status: 'grey',
-        name: 'Mojang Accounts Website',
-        essential: false
-    }
-]
+
 
 // Functions
 
